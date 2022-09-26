3139ead4be67220453f966db67a4b3b895ccc804
Neel Amonkar <s2030247@ed.ac.uk>
2022-09-26 17:32:04 +0100

    implemented getRestaurantsFromRestServer

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -1,6 +1,8 @@
 package uk.ac.ed.inf;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
 
 /**
  * Hello world!
@@ -21,5 +23,13 @@ public class App
         System.out.println(forSureInCentralArea.closeTo(new LngLat(-3.190005, 55.9430005)));
 
         System.out.println(forSureInCentralArea.nextPosition(CompassDirection.SOUTH));
+
+        var x = Restaurant.getRestaurantsFromRestServer(new URL("https://ilp-rest.azurewebsites.net/"));
+
+        System.out.println(Arrays.toString(x));
+
+        for (Restaurant r : x) {
+            System.out.println(Arrays.toString(r.getMenu()));
+        }
     }
 }
--- src/main/java/uk/ac/ed/inf/Restaurant.java
+++ src/main/java/uk/ac/ed/inf/Restaurant.java
@@ -1,10 +1,34 @@
 package uk.ac.ed.inf;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.MalformedURLException;
 import java.net.URL;
 
-public record Restaurant(String name, double longitude, double latitude, Menu[] menu) {
-    public static Restaurant[] getRestaurantsFromRestServer(URL serverBaseAddress) {
-        //@TODO: implement
-        return null;
+public class Restaurant {
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("longitude")
+    private double longitude;
+
+    @JsonProperty("latitude")
+    private double latitude;
+
+    @JsonProperty("menu")
+    private Menu[] menu;
+
+    public Restaurant() {
+    }
+
+    public Menu[] getMenu() { return menu; }
+
+    public static Restaurant[] getRestaurantsFromRestServer(URL serverBaseAddress) throws IOException {
+        return new ObjectMapper().readValue(new URL(serverBaseAddress.getProtocol() + "://" +
+                serverBaseAddress.getHost() + "/restaurants"), new TypeReference<>(){});
     }
 }
