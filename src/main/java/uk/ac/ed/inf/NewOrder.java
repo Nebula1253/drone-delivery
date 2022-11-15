f58e03bf6771140a57b95eb98e9d366e9613269f
Neel Amonkar <s2030247@ed.ac.uk>
2022-11-15 19:55:03 +0000

    added credit card validation, split validation method into sub-methods

--- src/main/java/uk/ac/ed/inf/NewOrder.java
+++ src/main/java/uk/ac/ed/inf/NewOrder.java
@@ -1,10 +1,10 @@
 package uk.ac.ed.inf;
 
+import java.io.IOException;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class NewOrder {
     private String orderNo, orderDate, customer, creditCardNumber, creditCardExpiry, cvv;
@@ -12,8 +12,11 @@ public class NewOrder {
     private ArrayList<String> orderItems;
     private OrderOutcome outcome = OrderOutcome.ValidButNotDelivered;
 
+    // constant value representing delivery fee
+    private static final int DELIVERY_FEE = 100;
+
     public NewOrder(String orderNo, String orderDate, String customer, String creditCardNumber, String creditCardExpiry,
-                    int priceTotalInPence, ArrayList<String> orderItems) {
+                    int priceTotalInPence, ArrayList<String> orderItems) throws IOException {
         this.orderNo = orderNo;
         this.orderDate = orderDate;
         this.customer = customer;
@@ -25,7 +28,14 @@ public class NewOrder {
         validate();
     }
 
-    private void validate() {
+    private void validate() throws IOException {
+        validateExpiryDate();
+        validateCVV();
+        validateOrderItems();
+        validateCreditCardNumber();
+    }
+
+    private void validateExpiryDate() {
         // Expiry date is invalid if: a) it's in the past, b) it's not in the "mm/yy" pattern
         try {
             LocalDate expiryDate = LocalDate.parse(this.creditCardExpiry, DateTimeFormatter.ofPattern("MM/yy"));
@@ -34,11 +44,79 @@ public class NewOrder {
             }
         }
         catch(Exception e) { this.outcome = OrderOutcome.InvalidExpiryDate; }
+    }
 
+    private void validateCVV() {
         // The CVV is invalid if it's longer than 3-4 characters and it contains non-numeric characters
-        Matcher m = Pattern.compile("^[0-9]{3,4}$").matcher(this.cvv);
-        if (!m.matches()) { this.outcome = OrderOutcome.InvalidCvv; }
+        if (!this.cvv.matches("^[0-9]{3,4}$")) { this.outcome = OrderOutcome.InvalidCvv; }
+    }
 
+    //TODO: there is probably a better way to do this
+    private void validateOrderItems() throws IOException {
+        int itemsRemaining = this.orderItems.size();
+        if (itemsRemaining > 4) { this.outcome = OrderOutcome.InvalidPizzaCount; }
+        Menu[] currentMenu;
+        int totalCost = 0;
+        boolean restaurantFound = false;
 
+        for (Restaurant r : Restaurant.getRestaurantsFromRestServer(new URL("https://ilp-rest.azurewebsites.net/"))) {
+            currentMenu = r.getMenu();
+
+            // iterate through every single menu item in the current restaurant
+            for (Menu m : currentMenu) {
+                // if this item has been ordered, add cost and flag that the restaurant has been found
+                if (this.orderItems.contains(m.name())) {
+                    totalCost += m.priceInPence();
+                    itemsRemaining--;
+                    restaurantFound = true;
+                }
+            }
+
+            // if you've finished iterating through all the items for this restaurant, found a match for one or more items in the order,
+            // and there are still items unaccounted for on the order, clearly something is wrong
+            if (restaurantFound && itemsRemaining != 0) {
+                this.outcome = OrderOutcome.InvalidPizzaCombinationMultipleSuppliers;
+            }
+        }
+        // if you've gone through ALL the menu items of all the participating restaurants, and there are still remaining items,
+        // those items are then invalid
+        if (itemsRemaining != 0) { this.outcome = OrderOutcome.InvalidPizzaNotDefined; }
+
+        // we've manually totalled up the price for all the items, including the delivery fee, so if this is inconsistent
+        // with the provided order total, we need to set the outcome accordingly
+        if (totalCost != this.priceTotalInPence) { this.outcome = OrderOutcome.InvalidTotal; }
+    }
+
+    private void validateCreditCardNumber(){
+        String[] creditCardDigits = this.creditCardNumber.split("");
+
+        if (this.creditCardNumber.matches("[0-9]+")) {
+            int firstTwoDigits = Integer.parseInt(creditCardDigits[0] + creditCardDigits[1]);
+            if (this.creditCardNumber.length() == 16 && (creditCardDigits[0].equals("4") || (firstTwoDigits >= 51 && firstTwoDigits <= 55))) {
+                // credit card validation: Luhn's algorithm
+                int sumOfDoubledDigits = 0, sumOfRemainingDigits = 0;
+                for (int i = creditCardDigits.length - 1; i >= 0; i--) {
+                    if (i % 2 == 0) {
+                        String[] doubledDigit = (Integer.toString(Integer.parseInt(creditCardDigits[i]) * 2)).split("");
+                        for (String s: doubledDigit) {
+                            sumOfDoubledDigits += Integer.parseInt(s);
+                        }
+                    }
+                    else {
+                        sumOfRemainingDigits += Integer.parseInt(creditCardDigits[i]);
+                    }
+                }
+                if ((sumOfDoubledDigits+sumOfRemainingDigits) % 10 != 0) {
+                    this.outcome = OrderOutcome.InvalidCardNumber;
+                }
+            }
+            else { this.outcome = OrderOutcome.InvalidCardNumber; }
+        }
+        else { this.outcome = OrderOutcome.InvalidCardNumber; }
+
+    }
+
+    public int getDeliveryCost() {
+        return this.priceTotalInPence + DELIVERY_FEE;
     }
 }
